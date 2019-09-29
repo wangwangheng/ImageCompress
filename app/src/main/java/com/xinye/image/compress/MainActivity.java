0d74ecdff184a33f1be5d28a528566d1ff098519
wangheng <wangheng@izuche.com>
2019-09-29 17:51:58 +0800

    图片压缩到Zip&重命名

--- app/src/main/java/com/xinye/image/compress/MainActivity.java
+++ app/src/main/java/com/xinye/image/compress/MainActivity.java
@@ -14,15 +14,24 @@ import com.zxy.tiny.Tiny;
 import com.zxy.tiny.callback.FileCallback;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function2;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView statusTextView;
+    private static final String FILE_EXT_POST = ".123";
+
+    private TextView mStatusTextView;
+    private EditText mCompressEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,51 +40,197 @@ public class MainActivity extends AppCompatActivity {
 
         requestAllPermission();
 
+        mStatusTextView = findViewById(R.id.tvStatus);
+
+        initCompressComponent();
+
+        initReplyComponent();
+
+        initZipComponent();
+
+    }
+
+    private void initZipComponent() {
+        final EditText zipEditText = findViewById(R.id.etZipPath);
+        Button btnZipSetDirName = findViewById(R.id.btnZipSetToOutputDir);
+        Button btnZip = findViewById(R.id.btnZip);
+
+        zipEditText.setText(mCompressEditText.getText());
+
+        btnZipSetDirName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                zipEditText.setText(mCompressEditText.getText());
+            }
+        });
+
+        btnZip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String path = zipEditText.getText().toString().trim();
+                if(path.length() <= 0){
+                    mStatusTextView.setText("请输入待压缩目录地址");
+                    return;
+                }
+                File file = new File(path);
+                if(!file.exists()){
+                    mStatusTextView.setText("文件路径不存在");
+                    return;
+                }
+
+                if(!file.isDirectory()){
+                    mStatusTextView.setText("地址不是一个目录");
+                    return;
+                }
 
 
-        statusTextView = findViewById(R.id.tvStatus);
-        Button button = findViewById(R.id.btnExecute);
+                final File[] list = file.listFiles();
+                if(list == null || list.length <= 0){
+                    mStatusTextView.setText("目录中没有文件");
+                    return;
+                }
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        final File outFile = new File(path,getDateString() + "-" + System.currentTimeMillis() + ".zip");
+                        ZipOutputStream os = null;
+                        try {
+                            os = new ZipOutputStream(new FileOutputStream(outFile));
+                            for(File f : list){
+                                String name = f.getName();
+                                os.putNextEntry(new ZipEntry(name));
+
+                                FileInputStream input = new FileInputStream(f);
+                                byte[] buf = new byte[1024];
+                                int len = -1;
+
+                                while ((len = input.read(buf)) != -1) {
+                                    os.write(buf, 0, len);
+                                }
+                                f.delete();
+                                final String filePath = f.getAbsolutePath();
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mStatusTextView.setText("写入文件完成:" + filePath);
+                                    }
+                                });
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }finally{
+                            try {
+                                if(os != null) {
+                                    os.close();
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mStatusTextView.setText("压缩到Zip文件成功:" + outFile.getAbsolutePath());
+                            }
+                        });
+                    }
+                }).start();
+
+            }
+        });
+    }
+
+    private void initReplyComponent() {
+        final EditText outputReplyEditText = findViewById(R.id.etOutputReplyPath);
+        outputReplyEditText.setText(mCompressEditText.getText());
+
+        Button btnReplySetDirName = findViewById(R.id.btnReplySetToOutputDir);
+        Button btnReply = findViewById(R.id.btnReply);
+        btnReplySetDirName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputReplyEditText.setText(mCompressEditText.getText());
+            }
+        });
+
+        btnReply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String path = outputReplyEditText.getText().toString().trim();
+                if(path.length() <= 0){
+                    mStatusTextView.setText("请输入目录地址");
+                    return;
+                }
+                File file = new File(path);
+                if(!file.exists()){
+                    mStatusTextView.setText("文件路径不存在");
+                    return;
+                }
+
+                if(!file.isDirectory()){
+                    mStatusTextView.setText("地址不是一个目录");
+                    return;
+                }
+
+
+                final File[] list = file.listFiles();
+                if(list == null || list.length <= 0){
+                    mStatusTextView.setText("目录中没有文件");
+                    return;
+                }
+
+                renameFileList(list,file);
+
+            }
+        });
+    }
+
+    private void initCompressComponent() {
         final EditText inputEditText = findViewById(R.id.etInputPath);
-        final EditText outputEditText = findViewById(R.id.etOutputPath);
-        final CheckBox checkBox = findViewById(R.id.cbDeleteSource);
+        mCompressEditText = findViewById(R.id.etOutputPath);
+        mCompressEditText.setText("/mnt/sdcard/DCIM/Camera-" + getDateString());
 
-        outputEditText.setText("/mnt/sdcard/DCIM/Camera-" + getDateString());
-
-        button.setOnClickListener(new View.OnClickListener() {
+        final CheckBox deleteCheckBox = findViewById(R.id.cbDeleteSource);
+        final CheckBox renameCheckBox = findViewById(R.id.cbRenameFile);
+        Button btnExecuteCompress = findViewById(R.id.btnExecute);
+        btnExecuteCompress.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String inputPath = inputEditText.getText().toString().trim();
-                String outputPath = outputEditText.getText().toString().trim();
+                String outputPath = mCompressEditText.getText().toString().trim();
                 if(inputPath.length() <= 0){
-                    statusTextView.setText("请输入输入目录地址");
+                    mStatusTextView.setText("请输入输入目录地址");
                     return;
                 }
 
                 File file = new File(inputPath);
                 if(!file.exists()){
-                    statusTextView.setText("输入目录地址不存在");
+                    mStatusTextView.setText("输入目录地址不存在");
                     return;
                 }
 
                 if(!file.isDirectory()){
-                    statusTextView.setText("输入地址不是一个目录");
+                    mStatusTextView.setText("输入地址不是一个目录");
                     return;
                 }
 
                 if(outputPath.length() <= 0){
-                    statusTextView.setText("请输入输出目录地址");
+                    mStatusTextView.setText("请输入输出目录地址");
                     return;
                 }
 
                 final File[] list = file.listFiles();
                 if(list == null || list.length <= 0){
-                    statusTextView.setText("输入目录中没有文件");
+                    mStatusTextView.setText("输入目录中没有文件");
                     return;
                 }
 
                 final File outDir = new File(outputPath);
                 if(!outDir.exists()){
-                    statusTextView.setText("创建输出目录失败");
+                    mStatusTextView.setText("创建输出目录失败");
                     outDir.mkdirs();
                 }
 
@@ -83,31 +238,63 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
-                compress(list,0,outDir,checkBox.isChecked());
+                compress(list,0,outDir,deleteCheckBox.isChecked(),renameCheckBox.isChecked());
 
             }
         });
     }
 
-    private void compress(final File[] list, final int index, final File outDir, final boolean delete){
+    private void renameFileList(final File[] list,final File dir) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String dirPath = dir.getAbsolutePath();
+                int count = list.length;
+                int index = 0;
+                for(File f : list){
+                    String name = f.getName();
+                    if(name.contains(FILE_EXT_POST)){
+                        name = name.replaceAll(FILE_EXT_POST,"");
+                        f.renameTo(new File(dirPath,name));
+                    }
+                    index++;
+                    final String text = "当前第" + index + "个文件重命名完成，共" + count + "个";
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mStatusTextView.setText(text);
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    private void compress(final File[] list, final int index, final File outDir, final boolean delete,final boolean rename){
         if(list == null || list.length == 0 || index < 0 || index > list.length - 1){
             return;
         }
         final File inputFile = list[index];
-        final File outFile = new File(outDir, inputFile.getName());
+        final String fileName = inputFile.getName();
+
+        final File outFile = new File(outDir, fileName);
 
         Tiny.FileCompressOptions options = new Tiny.FileCompressOptions();
-        options.quality = 90;
+        options.quality = 95;
         options.outfile = outFile.getAbsolutePath();
 
-        TextView tv = statusTextView;
+        TextView tv = mStatusTextView;
         Tiny.getInstance().source(inputFile.getAbsoluteFile()).asFile().withOptions(options).compress(new FileCallback() {
             @Override
             public void callback(final boolean isSuccess, String outfile, Throwable t) {
+                if(rename){
+                    String newFileName = fileName + FILE_EXT_POST;
+                    outFile.renameTo(new File(outDir,newFileName));
+                }
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        TextView tv = statusTextView;
+                        TextView tv = mStatusTextView;
                         if(tv != null){
                             if(index == list.length - 1){
                                 tv.setText("全部文件压缩完成(" + (index + 1) + "/" + list.length + ")");
@@ -118,7 +305,7 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 });
-                compress(list,index + 1,outDir,delete);
+                compress(list,index + 1,outDir,delete,rename);
                 if(isSuccess && delete){
                     inputFile.delete();
                 }
